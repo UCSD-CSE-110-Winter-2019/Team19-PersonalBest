d2ab4f724477426aab057e6eb1d4b6a007c3e5c4
xshi1012 <xshi@ucsd.edu>
2019-02-09 11:51:57 -0800

    fix bug

--- app/src/main/java/com/example/txwu/personalbest/WalkActivity.java
+++ app/src/main/java/com/example/txwu/personalbest/WalkActivity.java
@@ -3,7 +3,6 @@ package com.example.txwu.personalbest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-<<<<<<< HEAD
 import com.example.txwu.personalbest.fitness.FitAdapterForWalk;
 import com.example.txwu.personalbest.fitness.FitnessService;
 import com.example.txwu.personalbest.fitness.FitnessServiceFactory;
@@ -21,15 +20,11 @@ public class WalkActivity extends AppCompatActivity implements Observer {
 
     public Walk walk;
     private FitnessService fitnessService;
-=======
-public class WalkActivity extends AppCompatActivity {
->>>>>>> 4da31d8eb34be914c0d7650281f174f363ec602b
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
-<<<<<<< HEAD
 
         // create new Walk object using current time
         Calendar cal = Calendar.getInstance();
@@ -56,7 +51,5 @@ public class WalkActivity extends AppCompatActivity {
     public void update(Observable o, Object arg) {
         final long steps = (long)arg;
         setSteps(steps);
-=======
->>>>>>> 4da31d8eb34be914c0d7650281f174f363ec602b
     }
 }
