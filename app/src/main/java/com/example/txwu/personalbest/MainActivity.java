074ce0ecf42a164d10e095f98dbd7b6a54c15dc3
xshi1012 <xshi@ucsd.edu>
2019-02-14 13:45:56 -0800

    added a new Dialog

--- app/src/main/java/com/example/txwu/personalbest/MainActivity.java
+++ app/src/main/java/com/example/txwu/personalbest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.txwu.personalbest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
