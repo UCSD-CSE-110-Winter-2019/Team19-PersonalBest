06354663d46531f2a4f67f7ed99f9ced328da96b
xshi1012 <xshi@ucsd.edu>
2019-02-14 17:57:00 -0800

    added button to change goal

--- app/src/main/java/com/example/txwu/personalbest/EnterNewGoalDialogFragment.java
+++ app/src/main/java/com/example/txwu/personalbest/EnterNewGoalDialogFragment.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.Locale;
@@ -17,14 +18,16 @@ public class EnterNewGoalDialogFragment extends DialogFragment {
 
         // get current goal here, need modification
         int currentGoal = 5000;
+
         final int newGoal = Goal.suggestNextGoal(currentGoal);
         String suggested = String.valueOf(newGoal);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_change_goal, null);
 
         builder.setTitle(R.string.dialog_choose_goal)
-                .setView(inflater.inflate(R.layout.dialog_change_goal, null))
+                .setView(dialogView)
                 .setNegativeButton(R.string.dialog_not_now, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -35,12 +38,26 @@ public class EnterNewGoalDialogFragment extends DialogFragment {
                 .setPositiveButton(suggested, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
+                        // use suggested goal here
                         EnterNewGoalDialogFragment.this.getDialog().cancel();
                     }
                 })
                 .setNeutralButton(R.string.dialog_custom, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
+                        EditText customInput = dialogView.findViewById(R.id.custom_goal);
+                        int goal = newGoal;
+
+                        String input = customInput.getText().toString();
+                        try {
+                            goal = Integer.parseInt(input);
+                        }
+                        catch (NumberFormatException e) {
+                            goal = newGoal;
+                        }
+
                         EnterNewGoalDialogFragment.this.getDialog().cancel();
                     }
                 });
--- app/src/main/java/com/example/txwu/personalbest/MainActivity.java
+++ app/src/main/java/com/example/txwu/personalbest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -79,7 +80,6 @@ public class MainActivity extends AppCompatActivity implements Observer {
 
         StepsUpdateTask stepsUpdateTask = new StepsUpdateTask(this);
         stepsUpdateTask.addObserver(this);
-
     }
 
     private boolean isFirstTimeOpenApp = true;
@@ -155,5 +155,10 @@ public class MainActivity extends AppCompatActivity implements Observer {
                     GoogleSignIn.getLastSignedInAccount(this),
                     fitnessOptions);
         }
+    }
+
+    public void enterNewGoal(View view) {
+        DialogFragment enterNewGoalDialogFragment = new EnterNewGoalDialogFragment();
+        enterNewGoalDialogFragment.show(getSupportFragmentManager(), "new goal");
     }
 }
